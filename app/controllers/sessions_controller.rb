499a687fdc8db0d662e9951f02a605f81e73170d
Tyler Pearson <ty.pearson@gmail.com>
2013-07-03 23:14:12 -0400

    add signup flashes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,14 @@
 class SessionsController < ApplicationController
+
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to root_url
+    redirect_to root_url, notice: "Success! Thanks for adding your name!"
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url
+    redirect_to root_url, notice: "You are now signed out!"
   end
+
 end
