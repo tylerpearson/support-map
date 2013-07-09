fa14d9e443dc775579f083e1c658cd0f1e03c0aa
Tyler Pearson <ty.pearson@gmail.com>
2013-07-09 15:39:22 -0400

    add gender to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,12 @@ class User < ActiveRecord::Base
   after_validation :geocode
 
   def self.from_omniauth(auth)
-
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
       user.email = auth.info.email
+      user.gender = auth.info.gender
       user.nickname = auth.info.nickname
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
@@ -24,7 +24,6 @@ class User < ActiveRecord::Base
       user.save!
     end
   end
-
 
   def address
     if zip_code
