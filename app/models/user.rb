5d35a998f2c524f52ac2295dd08ca0b7c3ca5aca
Tyler Pearson <ty.pearson@gmail.com>
2013-07-09 13:25:49 -0400

    save email from facebook user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,12 @@ class User < ActiveRecord::Base
   after_validation :geocode
 
   def self.from_omniauth(auth)
+
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
+      user.email = auth.info.email
       user.nickname = auth.info.nickname
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, "355359997924701", "78f3afeb4cedf5880b0a6e26ffbcc827",
     :display => "page",
-    :scope => "email,user_about_me,user_interests,user_likes,user_location,friends_location,user_photos"
+    :scope => "email,user_about_me,user_location,friends_location"
 end
