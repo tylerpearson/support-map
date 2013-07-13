d67c9d9a0f3bff42c8f96c6a3fdcb98c45d4936e
Tyler Pearson <ty.pearson@gmail.com>
2013-07-13 17:31:33 -0400

    add validations for friends

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -3,5 +3,6 @@ class Friend < ActiveRecord::Base
   belongs_to :location
   has_many :invitations
 
+  validates :first_name, :last_name, :name, :location_id, :username, :gender, :uid, :presence => true
 
 end
